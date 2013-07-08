62bc2d50af74de910b1c26c0cb04c064cc96fe50
danlee6 <danlee6@yahoo.com>
2013-07-07 23:51:09 -0400

    #3 - created some semi-static pages for EMSI, EBI, CEOM

--- src/main/java/org/emsionline/emsiweb/web/controller/SemiStaticPageController.java
+++ src/main/java/org/emsionline/emsiweb/web/controller/SemiStaticPageController.java
@@ -1,5 +1,10 @@
 package org.emsionline.emsiweb.web.controller;
 
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -12,8 +17,38 @@ public class SemiStaticPageController {
 	
 	final Logger logger = LoggerFactory.getLogger(SemiStaticPageController.class);
 	
-	@RequestMapping("/emsi/intro")
-	public String emsiIntro(Model model) {
-		return "emsi/intro";
+	@RequestMapping({"/emsi"})
+	public String partialPathEMSI() {
+		return "redirect:/emsi/intro";
+	}
+	
+	@RequestMapping({"/ebi"})
+	public String partialPathEBI() {
+		return "redirect:/ebi/america";
+	}
+	
+	@RequestMapping({"/ceom"})
+	public String partialPathCEOM() {
+		return "redirect:/ceom/intro";
+	}
+	
+	@RequestMapping({"/emsi/*", "/ebi/*", "/ceom/*"})
+	public String serveSemiStaticContent(
+			Model model
+			, HttpServletRequest request
+			, Locale locale
+			) {
+		String[] pathElements = getPathElements(request.getServletPath());
+		String lang = locale.getLanguage();
+		StringBuilder strbld = new StringBuilder();
+		strbld.append("semistatic").append("/")
+			.append(pathElements[0]).append("/")
+			.append(pathElements[1]).append("_").append(lang);
+		return strbld.toString();
+	}
+	
+	
+	private String[] getPathElements(String servletPath) {
+		return StringUtils.split(servletPath, "/");
 	}
 }
--- src/main/webapp/scripts/app/app.js
+++ src/main/webapp/scripts/app/app.js
@@ -1,16 +1,71 @@
 var App = function() {
 	
+	// returns true if str1 starts with str2
+	var _startsWith = function(str1, str2) {
+		try {
+			return str1.indexOf(str2) == 0;
+		} catch (e) {
+			return false;
+		}
+	};
+	
+	// Setup selected class on left nav.
+	// Will add the 'selected' class to the <li> element if the child <a> href attribute matches current path.
+	var _setupLeftNavSelectedItem = function() {
+		var win_pathname = window.location.pathname;
+		$('#page-nav li a').each(function (index) {
+			var $this = $(this);
+			$this.parent().removeClass('selected'); // first initialize all to unselected.
+			var href = $this.attr('href');
+			if (_startsWith(win_pathname, href)) { // see if this href matches the browser's current path
+				$this.parent().addClass('selected');
+			}
+		});
+	};
+	
+	// initialize the change-lang-?? link
+	var _setupChangeLangLink = function() {
+		$('#change-lang-zh').click(function(evt) {
+			_onClickChangeLang('zh');
+			evt.preventDefault();
+		});
+		$('#change-lang-en').click(function(evt) {
+			_onClickChangeLang('en');
+			evt.preventDefault();
+		});
+	};
+	
+	var _onClickChangeLang = function(lang) {
+		var pathname = window.location.pathname;
+		var search = window.location.search;
+		search = _adjustSearch(search);
+		window.location = pathname+"?lang="+lang;
+	};
+	
+	var _adjustSearch = function(search) {
+		if (search.indexOf("lang=zh") > -1) {
+			return ""; // TODO: change this to replace the string "lang=zh" 
+		}
+		if (search.indexOf("lang=en") > -1) {
+			return ""; // TODO: change this to replace the string "lang=en"
+		}
+		return search;
+	};
+	
+	// setup logo .click()
+	var _setupLogoClick = function(initObj) {
+		$('#logo').css('cursor', 'pointer');
+		$('#logo').click(function(evt) {
+			window.location = initObj.homeUrl;
+		});
+	};
+	
 	
 	return {
-		init: function () {
-			$('#page-nav li').click(function (evt) {
-				$('#page-nav li').removeClass('selected');
-				var $this = $(this);
-				if (!$this.hasClass('selected')) {
-					$this.addClass('selected');
-				}
-				evt.preventDefault();
-			});
+		init: function (initObj) {
+			_setupLeftNavSelectedItem();
+			_setupChangeLangLink();
+			_setupLogoClick(initObj);
 		}
 	};
 }();
