abe6f2a8e20c17891f21f7b6a764de5e2928bd12
chuiau <chuiau@hotmail.com>
2015-09-03 00:41:56 -0400

    236: 111-114 added new church in Portugal.  CEC in Azores

--- src/main/resources/emsi-church-content-new.sql
+++ src/main/resources/emsi-church-content-new.sql
@@ -3093,6 +3093,189 @@ update church_content set body = STRINGDECODE('<p>
 
 -----------------ends Portugal - Mindelo
 
+---------- Portugal - Azores  New church Sept 2015 ---------
+INSERT INTO CHURCH(CHURCH_ID, CHURCH_PATH, ENABLED, SORT_ORDER, VERSION) VALUES(57, 'azores', 1, 5, 0);               
+INSERT INTO PUBLIC.CHURCH_DETAIL(CHURCH_ID, LOCALE, KEY, VALUE) VALUES(57, 'en', 'menu_name', 'CEC in Azores');             
+INSERT INTO PUBLIC.CHURCH_DETAIL(CHURCH_ID, LOCALE, KEY, VALUE) VALUES(57, 'zh', 'menu_name', '亞速爾基督教生命堂 ');            
+INSERT INTO PUBLIC.CHURCH_HIERARCHY(PARENT_ENTITY_ID, CHURCH_ORG_ID, CHURCH_ID) VALUES(9, NULL, 57);          
+
+INSERT INTO PUBLIC.CHURCH_CONTENT(CHURCH_ID, PAGE_ID, LOCALE, TITLE, BODY) VALUES(57, 'intro', 'en', 'CEC in Azores  - Brief History', '<style type="text/css">.contact-content {font-family: Arial,helvetica,sans-serif;}
+.contact-content td {font-size: small; padding: 2px; 3px;}
+td.label {font-weight: bold; width: 1%; white-space: nowrap;}
+p span.label {font-weight: bold;}
+.main-content {font-family: Arial, MingLiU, Helvetica, san-serif; font-size: 13px; color: #333;}
+.borderless-table {border-width: 0px;}
+</style>
+<div class="contact-content">
+<div style="color: #000000; font-size: small">
+<table cellpadding="0px" cellspacing="0px">
+	<tbody class="borderless-table">
+		<tr>
+			<td class="label"> </td>
+			<td></td>
+		</tr>
+
+	</tbody>
+</table>
+
+<div style="margin-left: 20px">
+	<table cellpadding="1px" cellspacing="0px">
+		<tbody class="borderless-table">
+			<tr>
+				<td class="label"><img height="16" src="/emsiweb/images/emsi/icon_googlemap.gif" width="16" /></td>
+				<td class="label">Address:</td>
+				<td>Rua do Mercado nº50 9500-326 Ponta Delgada, Portugal</td>
+			</tr>
+			<tr>
+				<td class="label"><img height="16" src="/emsiweb/images/emsi/icon_phone.gif" width="16" /></td>
+				<td class="label">Tel#:</td>
+				<td>  (cell) 351-963-333-777 </td>
+			</tr>
+			<tr>
+				<td><img height="16" src="/emsiweb/images/emsi/icon_email.gif" width="16" /></td>
+				<td class="label">Email:</td>
+				<td><a href=\"mailto: emsiacores@hotmail.com"> emsiacores@hotmail.com</a></td>			
+			</tr>
+		</tbody>
+	</table>
+</div>
+</div>
+
+<hr align="center" size="1" width="100%" /></div>
+
+<div class="main-content">
+	<p><span class="label">Established Date: </span>Sept 2015</p>
+	<p>
+	</p>
+	<p>
+
+	</p>
+
+</div>
+');
+
+
+
+INSERT INTO PUBLIC.CHURCH_CONTENT(CHURCH_ID, PAGE_ID, LOCALE, TITLE, BODY) VALUES(57, 'intro', 'zh', '亞速爾基督教生命堂 - 教會簡介', '<style type="text/css">.contact-content {font-family: Arial,helvetica,sans-serif,新細明體 ;}
+.contact-content td {font-size: 14px; padding: 2px; 3px;}
+td.label {font-weight: bold; width: 1%; white-space: nowrap;}
+p span.label {font-weight: bold;}
+.main-content {font-family: Arial, MingLiU, Helvetica, san-serif, 新細明體; font-size: 13px; color: #333;}
+.borderless-table {border-width: 0px;}
+</style>
+<div class="contact-content">
+<div style="color: #000000; font-size: small">
+<table cellpadding="0" cellspacing="0">
+	<tbody class="borderless-table">
+		<tr>
+			<td class="label"></td>
+			<td></td>
+		</tr>
+	</tbody>
+</table>
+&nbsp;
+
+<div style="margin-left: 20px">
+	<table cellpadding="1" cellspacing="0">
+		<tbody class="borderless-table">
+			<tr>
+				<td class="label"><img height="16" src="/emsiweb/images/emsi/icon_googlemap.gif" width="16" /></td>
+				<td class="label">地址:</td>
+				<td>Rua do Mercado nº50 9500-326 Ponta Delgada, Portugal</td>
+			</tr>
+			<tr>
+				<td class="label"><img height="16" src="/emsiweb/images/emsi/icon_phone.gif" width="16" /></td>
+				<td class="label">電話:</td>
+				<td> (手機) 351-963-333-777</td>
+			</tr>
+			<tr>
+				<td><img height="16" src="/emsiweb/images/emsi/icon_email.gif" width="16" /></td>
+				<td class="label">電郵:</td>
+				<td><a href=\"mailto: emsiacores@hotmail.com"> emsiacores@hotmail.com</a></td>			
+			</tr>
+		</tbody>
+	</table>
+</div>
+</div>
+
+<hr align="center" size="1" width="100%" /></div>
+
+<div class="main-content">
+<p>
+<span class=\"label\">成立日期: </span>2015 年 9 月 
+</p>
+<p>
+<span class=\"label\">簡史: </span><br />
+<p>
+</p>
+<p>
+</p>
+</div>');
+
+INSERT INTO PUBLIC.CHURCH_CONTENT(CHURCH_ID, PAGE_ID, LOCALE, TITLE, BODY) VALUES(57, 'contactus', 'en', 'CEC in Azores - Contact Us', 'Christian Evangelical Church in Azores<br><br>');          
+INSERT INTO PUBLIC.CHURCH_CONTENT(CHURCH_ID, PAGE_ID, LOCALE, TITLE, BODY) VALUES(57, 'contactus', 'zh', '亞速爾基督教生命堂  - 連絡我們','');          
+INSERT INTO PUBLIC.CHURCH_CONTENT(CHURCH_ID, PAGE_ID, LOCALE, TITLE, BODY) VALUES(57, 'schedule', 'en', 'CED in Azores - Church Services Schedule', '<table border="1" cellpadding="1" cellspacing="1" style="width: 500px;">
+	<tbody>
+		<tr>
+			<td>Sunday Worship</td>
+			<td></td>
+			<td></td>
+		</tr>
+	</tbody>
+</table>
+
+<p>&nbsp;</p>
+');            
+INSERT INTO PUBLIC.CHURCH_CONTENT(CHURCH_ID, PAGE_ID, LOCALE, TITLE, BODY) VALUES(57, 'schedule', 'zh', '亞速爾基督教生命堂  - 聚會日程', '<table border="1" cellpadding="1" cellspacing="1" style="width: 500px;">
+	<tbody>
+		<tr>
+			<td>主日崇拜 </td>
+			<td></td>
+			<td></td>
+		</tr>
+	</tbody>
+</table>
+
+<p>&nbsp;</p>
+');             
+
+
+update church set address='Rua do Mercado nº50 9500-326 Ponta Delgada, Portugal', latitude = 37.7422794, longitude = -25.6632306 where church_path = 'azores';
+
+update church_content set body = STRINGDECODE('
+<style>
+.indentation {margin:20px;}
+</style>
+<p>
+<b>Address: </b>Rua do Mercado nº50 9500-326 Ponta Delgada, Portugal</p>
+<p>
+<b>Tel #: </b> (cell) 351-963-333-777</p>
+<p>
+<b>Email: </b> emsiacores@hotmail.com</p>
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3155.0368632807786!2d-25.663230600000016!3d37.742279400000015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xb432acaaea24f5f%3A0x56f24e3346ed13f6!2sR.+do+Mercado+50%2C+9500+Ponta+Delgada%2C+Portugal!5e0!3m2!1sen!2sus!4v1441251420358" width="640" height="480" frameborder="0" style="border:0" allowfullscreen></iframe>
+</p>
+')	where church_id = (select church_id from church where church_path = 'azores')
+	and locale = 'en'
+	and page_id = 'contactus';
+	
+update church_content set body = STRINGDECODE('<p>
+<p><b>地 址: </b>Rua do Mercado nº50 9500-326 Ponta Delgada, Portugal</p>
+<p>
+<b>電 話: </b>(手機) 351-963-333-777</p>
+<p>
+<b>電 郵: </b>emsiacores@hotmail.com</p>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3155.0368632807786!2d-25.663230600000016!3d37.742279400000015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xb432acaaea24f5f%3A0x56f24e3346ed13f6!2sR.+do+Mercado+50%2C+9500+Ponta+Delgada%2C+Portugal!5e0!3m2!1sen!2sus!4v1441251420358" width="640" height="480" frameborder="0" style="border:0" allowfullscreen></iframe>
+</p>
+')	where church_id = (select church_id from church where church_path = 'azores')
+	and locale = 'zh'
+	and page_id = 'contactus';
+
+-----------------------end Portugal - Azores
+	
+	
+
+	
 -----------------starts Hungary - budapest
 
 update church_content set body = '<style>
