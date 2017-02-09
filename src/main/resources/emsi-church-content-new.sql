dd30052d0a3c10eb836c0a6398383495d485fb46
chuiau <chuiau@hotmail.com>
2017-02-09 11:09:14 -0500

    314 - updated queens address

--- src/main/resources/emsi-church-content-new.sql
+++ src/main/resources/emsi-church-content-new.sql
@@ -269,6 +269,7 @@ update church_content set body = '<table border="1" cellpadding="5">
 	
 		
 ---------------------update queens pages
+---------------------updated address 2-8-17
 
 update church_content set body = '<style>
 .contact-content {font-family: Arial,helvetica,sans-serif;}
